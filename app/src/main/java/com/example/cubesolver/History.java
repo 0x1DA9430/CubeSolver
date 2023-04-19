04f1963647c4141da4109813b6e79851d701ed86
Ke Yan <yank.fitzgerald@gmail.com>
2023-04-19 21:46:09 +0100

    Bug Fix on History page

--- app/src/main/java/com/example/cubesolver/History.java
+++ app/src/main/java/com/example/cubesolver/History.java
@@ -28,64 +28,67 @@ public class History extends AppCompatActivity {
         LinearLayout historyContainer = findViewById(R.id.ll_history_container);
 
         // Find the minimum result
-        String minResult = Collections.min(sortedResultsHistory.values());
+        if (!sortedResultsHistory.isEmpty()) {
+            String minResult = Collections.min(sortedResultsHistory.values());
 
-        for (Map.Entry<String, String> entry : sortedResultsHistory.entrySet()) {
-            String dateTime = entry.getKey();
-            String result = entry.getValue();
-            boolean isBest = result.equals(minResult);
+            for (Map.Entry<String, String> entry : sortedResultsHistory.entrySet()) {
+                String dateTime = entry.getKey();
+                String result = entry.getValue();
+                boolean isBest = result.equals(minResult);
 
-            TextView dateTimeTextView = new TextView(this);
-            dateTimeTextView.setText(dateTime);
-            dateTimeTextView.setTextSize(16);
-            dateTimeTextView.setTextColor(Color.BLACK);
+                TextView dateTimeTextView = new TextView(this);
+                dateTimeTextView.setText(dateTime);
+                dateTimeTextView.setTextSize(16);
+                dateTimeTextView.setTextColor(Color.BLACK);
 
-            TextView resultTextView = new TextView(this);
-            resultTextView.setText(result);
-            resultTextView.setTextSize(16);
-            resultTextView.setTextColor(Color.BLACK);
-            resultTextView.setGravity(Gravity.END);
+                TextView resultTextView = new TextView(this);
+                resultTextView.setText(result);
+                resultTextView.setTextSize(16);
+                resultTextView.setTextColor(Color.BLACK);
+                resultTextView.setGravity(Gravity.END);
 
-            // LinearLayout as a container
-            LinearLayout rowLayout = new LinearLayout(this);
-            rowLayout.setOrientation(LinearLayout.HORIZONTAL);
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(0, 20, 0, 20);
-            rowLayout.setLayoutParams(layoutParams);
+                // LinearLayout as a container
+                LinearLayout rowLayout = new LinearLayout(this);
+                rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins(0, 20, 0, 20);
+                rowLayout.setLayoutParams(layoutParams);
 
-            // Set weight to show them in one line
-            LinearLayout.LayoutParams dateTimeLayoutParams = new LinearLayout.LayoutParams(
-                    0, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
-            LinearLayout.LayoutParams resultLayoutParams = new LinearLayout.LayoutParams(
-                    0, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
-            dateTimeTextView.setLayoutParams(dateTimeLayoutParams);
-            resultTextView.setLayoutParams(resultLayoutParams);
+                // Set weight to show them in one line
+                LinearLayout.LayoutParams dateTimeLayoutParams = new LinearLayout.LayoutParams(
+                        0, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+                LinearLayout.LayoutParams resultLayoutParams = new LinearLayout.LayoutParams(
+                        0, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+                dateTimeTextView.setLayoutParams(dateTimeLayoutParams);
+                resultTextView.setLayoutParams(resultLayoutParams);
 
 
-            // Add Date/Time TextView
-            rowLayout.addView(dateTimeTextView);
-            // Add blue border if it's the best result
-            if (isBest) {
-                rowLayout.setBackgroundColor(Color.rgb(232, 245, 255));
-                rowLayout.setPadding(0, 0, 0, 10);
-                TextView bestTextView = new TextView(this);
-                bestTextView.setText("Best!");
-                bestTextView.setTextSize(14);
-                bestTextView.setTextColor(Color.rgb(115, 187, 243));
-                bestTextView.setTypeface(null, Typeface.BOLD);
-                bestTextView.setGravity(Gravity.END | Gravity.TOP);
-                rowLayout.addView(bestTextView);
+                // Add Date/Time TextView
+                rowLayout.addView(dateTimeTextView);
+                // Add blue border for the best result
+                if (isBest) {
+                    rowLayout.setBackgroundColor(Color.rgb(232, 245, 255));
+                    rowLayout.setPadding(0, 0, 0, 10);
+                    TextView bestTextView = new TextView(this);
+                    bestTextView.setText("Best!");
+                    bestTextView.setTextSize(14);
+                    bestTextView.setTextColor(Color.rgb(115, 187, 243));
+                    bestTextView.setTypeface(null, Typeface.BOLD);
+                    bestTextView.setGravity(Gravity.END | Gravity.TOP);
+                    rowLayout.addView(bestTextView);
+                }
+                // Add Result TextView
+                rowLayout.addView(resultTextView);
+
+
+                // Add a row and a divider
+                historyContainer.addView(rowLayout);
+                historyContainer.addView(createDivider());
             }
-            // Add Result TextView
-            rowLayout.addView(resultTextView);
-
-
-            // Add a row and a divider
-            historyContainer.addView(rowLayout);
-            historyContainer.addView(createDivider());
         }
+
 
     }
 
